d1ada0d79a6bc7adf0085d1a64e46c9b76d39d59
jochemvandooren <jochemvandooren@hotmail.nl>
2016-11-10 14:44:48 +0100

    upload china code

--- China/China.py
+++ China/China.py
@@ -1,5 +1,33 @@
 import pickle
+from random import shuffle
 with open('China.pickle', 'rb') as handle:
   China = pickle.load(handle)
 
 print China
+
+towers = ['A','B','C','D','E','F','G']
+colours = {'A': '#7F0000', 'B':'#FF4C4C','C': '#FF0000','D':'#7F2626', 'E':'#CC0000', 'F':'green', 'G':'blue'}
+towerdict = {}
+indices = range(1,len(China)+1)
+
+def calculate_towers(indices): 
+    shuffle(indices)
+	#loop through all the provinces      
+    for i in indices:
+        towers2 = ['A','B','C','D','E','F','G']
+        neighbours = China[i]
+
+        #remove possible towers based on neighbours
+        for neighbour in neighbours:
+            if neighbour in towerdict.keys():
+                if towerdict[neighbour] in towers2:
+                    towers2.remove(towerdict[neighbour])
+    
+        #get first tower from list    
+        towerdict[i] = towers2[0]
+                
+    return towerdict
+    
+print 'E' in calculate_towers(indices).values()
+
+
